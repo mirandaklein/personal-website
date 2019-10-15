2d554a36414b5a2a61388f4447f013cf637287d3
mirandaklein <mirandaklein@mac.com>
2019-10-15 10:10:31 -0600

    project art maker

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,6 +6,8 @@ class Projects extends Component{
             <div className="projects-container">
                 <li className="project a"><a href="https://mirandaklein.github.io/neighborhood-maps/" rel="noopener noreferrer" target="_blank">Knoxville Bars</a></li>
                 <li className="project a"><a href="https://mirandaklein.github.io/ArcadeGame/" rel="noopener noreferrer" target="_blank">Arcade Game</a></li>
+                <li className="project a"><a href="https://mirandaklein.github.io/Pixel-Art-Maker/" rel="noopener noreferrer" target="_blank">Art Designer</a></li>
+
             </div>
         )
     }
